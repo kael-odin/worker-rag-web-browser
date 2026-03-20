b23b21a041e7e4bb9562e8a977c141ad7cca8b23
Thordata <thordata.team@gmail.com>
2026-03-20 18:14:14 +0800

    Simplify input_schema to only required field

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ async function run() {
         await cafesdk.log.debug(`Input parameters: ${JSON.stringify(inputJson)}`)
 
         const queryArray = inputJson?.query || []
-        const query = queryArray[0] || ''
+        const query = queryArray[0]?.url || ''
         const maxResults = inputJson?.maxResults || 3
         const outputFormat = inputJson?.outputFormat || 'markdown'
         const scrapingTool = inputJson?.scrapingTool || 'raw-http'
